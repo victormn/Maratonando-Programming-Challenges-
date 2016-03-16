f7958ed85c4d5be13ed544d938963e94d1bc0039
Victor Nunes <victor.marcelino.nunes@usp.br>
2016-03-15 23:30:18 -0300

    1.6.1 testado no uDebug, não submetido no UVa (site fora do ar)

---
+++ 1-6-1-The 3n+1 Problem (UVa: 100).c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int	main(){
+
+	int first1, first2, last1, last2, n, size, counter, max;
+	int i = 0;
+
+	while(scanf("%d %d", &first1, &last1)){
+
+		if (first1 > last1){
+			first2 = last1;
+			last2 = first1;
+		}
+		else{
+			first2 = first1;
+			last2 = last1;
+		}
+
+		max=0;
+
+		for(i=first2; i<=last2; i++){
+
+			n = i;
+			counter = 0;
+
+			while(n!=1){
+
+				if (n%2 == 0) // par
+					n /= 2;
+				else // impar
+					n = (n*3) + 1;
+
+				counter++;
+
+			}
+			if (counter > max)
+				max = counter;
+		}
+
+		printf("%d %d %d\n", first1, last1, max+1);
+	}	
+
+	return 0;
+}
